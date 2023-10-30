9f4d94bc8f4b6efc7f6a14caa929e902b22f1341
yoni-85 <141345368+yoni-85@users.noreply.github.com>
2023-10-29 19:26:01 -0600

    mi primer commit

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,11 +3,14 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array[0];
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+   // console.log(array[array.length - 1]);  
+   return array[array.length - 1];
 }
 
 function obtenerLargoDelArray(array) {
