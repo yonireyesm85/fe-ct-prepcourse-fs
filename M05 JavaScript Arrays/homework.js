086911f85e938edc986a327d0867f9b94b6024dd
Your Name <you@example.com>
2024-01-08 09:00:28 -0600

    mi primer commit

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -84,12 +84,19 @@ function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   return agregarNumeros(resultadosTest) / resultadosTest.length
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   // return Math.max(...arrayOfNums)
+   for(var i = 0 ; i < arrayOfNums.length ; i++){
+      if(arrayOfNums[i] > arrayOfNums[i + 1]){
+         return arrayOfNums[i]
+       }
+    }
 }
 
 function multiplicarArgumentos() {
@@ -97,11 +104,25 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   if(arguments.length === 0) return 0;
+   var multiplicar = 1;
+   
+   for(var i = 0; i < arguments.length; i++){
+      multiplicar = multiplicar * arguments[i]
+   }
+   return multiplicar;
 }
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+   var contador = 0;
+   for(var i = 0; i < array.length; i++){
+      if(array[i] > 18){
+         contador++;
+      }
+   }
+   return contador;
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -109,18 +130,30 @@ function diaDeLaSemana(numeroDeDia) {
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
    // Tu código:
+   if(numeroDeDia === 1 || numeroDeDia === 7) return "Es fin de semana"
+   else return "Es dia laboral"
 }
 
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar true si el entero inicia con 9 y false en otro caso.
    // Tu código:
+   num = num.toString()
+
+   if(num.charAt(0) === '9') return true;
+   else return false;
 }
+
+// console.log(empiezaConNueve(954));    
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
+   for(var i = 0; i < array.length ; i++){
+      if(array[i] === array[i + 1]) return true;
+      else return false;
+   }
 }
 
 function mesesDelAño(array) {
